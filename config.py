3c8609918f603e7d9ae85f22f8df66b0fbcde2a6
soulxmusicbot <rustydragon20@gmail.com>
2024-08-24 19:57:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002179927049"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7013604559"))
 
 #Port
-PORT = os.environ.get("PORT", "3000")
+PORT = os.environ.get("PORT", "7000")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
